09afa4017ff4d9a4203b3ddb7d90ab6e5c44abfe
kshammer <hammerschmiks@vcu.edu>
2018-01-29 13:48:45 -0500

    Added removal of bad data

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ test9 = []
 #reads the hero data and puts it in the dictionary heroInfo
 def readHeroData():
     with open("../felicityChallenge/hero_data.csv") as data:
-        lines = data.read().split('\n')[1:-1]
+        lines = data.read().split('\n')[1:-1]#remove first and last entry
         for line in lines:
             tokens = line.split(',')
             heroInfo[tokens[0]] = []
@@ -64,15 +64,16 @@ def normalizeKDA():
     min = 999999.0
     max = 0.0
     for player in train9:
-        if float(player[5]) < min:
-            min = float(player[5])
-        if float(player[5]) > max:
-            max = float(player[5])
+        if int(player[3]) > 10:
+            if float(player[5]) < min:
+                min = float(player[5])
+            if float(player[5]) > max:
+                max = float(player[5])
     bottom = max - min
-    print(max)
     for player in train9:
-        top = float(player[5]) - min
-        player.append(top/bottom)
+        if int(player[3]) > 10:
+            top = float(player[5]) - min
+            player.append(top/bottom)
 
 
 def graphData():
@@ -89,11 +90,13 @@ def graphData():
 def graphDataNormal():
     x = []
     y = []
+    #adding emtpy arrays
     for z in range(1, 122):
         y.append([])
     for player in train9:
         x.append(player[1])
-        y[int(player[1]) - 1].append(player[6])
+        if len(player) == 7:
+            y[int(player[1]) - 1].append(player[6])
     for xe, ye in zip(x, y):
         plt.scatter([xe] * len(ye), ye)
     plt.show()
